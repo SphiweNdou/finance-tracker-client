82996a14adb8486f4ec70c85a6f9dd47a585da3d
Sphiwe Ndou <ndebondou@gmail.com>
2025-02-02 12:21:19 +0200

    Configured account component to use account service and list accounts on ui

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './accounts.component.html',
-  styleUrl: './accounts.component.scss'
+  styleUrl: './accounts.component.scss',
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit {
+  accounts: any[] = [];
 
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.accountService.getAccounts().subscribe({
+      next: (data) => (this.accounts = data),
+      error: (err) => console.error('Error fetching accounts:', err),
+    });
+  }
 }
